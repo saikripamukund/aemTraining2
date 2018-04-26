d0cc5b309d35e6ac5e5bf413ebe80a5fbb2eb50a
Mukund <saikripa.mukund@accenture.com>
2018-04-26 15:39:14 -0700

    footer card header expand css updates

--- ui.apps/src/main/content/jcr_root/apps/aemTraining2/components/content/navigation/clientlib/navbar.js
+++ ui.apps/src/main/content/jcr_root/apps/aemTraining2/components/content/navigation/clientlib/navbar.js
@@ -6,7 +6,7 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementsByClassName("navbar");
 
 // Get the offset position of the navbar
-var sticky = offsetTop;
+var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 
